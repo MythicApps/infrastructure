ef205e5441a9023c3dafafbf4a7f4675b1aef45d
Andrew Tomaka <atomaka@gmail.com>
2015-06-09 18:05:13 -0400

    Redirect www to non-www and non-ssl to ssl

--- manifests/proxy.pp
+++ manifests/proxy.pp
@@ -18,9 +18,31 @@ nginx::resource::vhost { 'api.mythicapps.io':
   proxy => 'http://api',
 }
 
+nginx::resource::vhost { 'mythicapps.io-non-www':
+  ensure              => present,
+  server_name         => ['mythicapps.io', 'www.mythicapps.io'],
+  proxy               => 'http://static',
+  location_cfg_append => {
+      'rewrite' => '^ https://mythicapps.io$request_uri? permanent'
+  },
+}
+
+nginx::resource::vhost { 'www.mythicapps.io':
+  ensure              => present,
+  listen_port         => 443,
+  proxy               => 'http://static',
+  location_cfg_append => {
+      'rewrite' => '^ https://mythicapps.io$request_uri? permanent'
+  },
+  ssl         => true,
+  ssl_cert    => '/etc/ssl/certs/www.mythicapps.io-bundle.crt',
+  ssl_key     => '/etc/ssl/private/www.mythicapps.io.key',
+}
+
 nginx::resource::vhost { 'mythicapps.io':
-  server_name => ['mythicapps.io', 'www.mythicapps.io'],
+  ensure      => present,
   proxy       => 'http://static',
+  listen_port => 443,
   ssl         => true,
   ssl_cert    => '/etc/ssl/certs/www.mythicapps.io-bundle.crt',
   ssl_key     => '/etc/ssl/private/www.mythicapps.io.key',
